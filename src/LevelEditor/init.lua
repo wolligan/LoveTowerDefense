a47c5e35619cad84658726251959581ef60c635c
Steve Wolligandt <wolligan@st.ovgu.de>
2014-05-23 18:24:19 +0200

    LevelEditor now shows which tile is selected

--- src/LevelEditor/init.lua
+++ src/LevelEditor/init.lua
@@ -19,7 +19,6 @@ end
 function LevelEditor.createGUI()
     LevelEditor.GUI = GUI.Container(nil,{255,255,255,200},nil,{200,200,200,200},{255,255,255,200},{50,50,50,255},Utilities.Color.black)
 
-
     -- create a textfield
     local tf_mapName = GUI.Textfield("Map Name")
     tf_mapName:setLeftAnchor(GUI.Root, "right")
@@ -29,7 +28,6 @@ function LevelEditor.createGUI()
     tf_mapName.bottomAnchorOffset = 40
     tf_mapName.leftAnchorOffset = -150
     tf_mapName.rightAnchorOffset = -10
-
 
     local button_saveMap = GUI.Button("Save Map", function() Tilemap.getActiveScene():saveMap(tf_mapName.text .. ".map") end)
     button_saveMap:setTopAnchor(GUI.Root, "bottom")
@@ -47,11 +45,20 @@ function LevelEditor.createGUI()
     button_loadMap.leftAnchorOffset = 5
     button_loadMap.rightAnchorOffset = -10
 
+    local widget_selectedTile = GUI.Widget()
+    widget_selectedTile:setTopAnchor(button_loadMap, "top")
+    widget_selectedTile:setBottomAnchor(button_loadMap, "top")
+    widget_selectedTile:setRightAnchor(tf_mapName, "right")
+    widget_selectedTile:setLeftAnchor(tf_mapName, "left")
+    widget_selectedTile.topAnchorOffset = -70
+    widget_selectedTile.bottomAnchorOffset = -10
+    widget_selectedTile.render = LevelEditor.renderSelectedTileWidget
+
 -- create tile button list
-    local list_tiles = GUI.List("horizontal", 40, 1)
+    local list_tiles = GUI.List("horizontal", 30, 1)
     list_tiles:setLeftAnchor(tf_mapName, "left")
     list_tiles:setRightAnchor(tf_mapName, "right")
-    list_tiles:setBottomAnchor(button_loadMap, "top")
+    list_tiles:setBottomAnchor(widget_selectedTile, "top")
     list_tiles:setTopAnchor(tf_mapName, "bottom")
     list_tiles.topAnchorOffset = 10
 
@@ -67,6 +74,7 @@ function LevelEditor.createGUI()
 
     list_tiles:addWidgetsToContainer(LevelEditor.GUI)
     LevelEditor.GUI:addWidget(emptyField)
+    LevelEditor.GUI:addWidget(widget_selectedTile)
     LevelEditor.GUI:addWidget(button_saveMap)
     LevelEditor.GUI:addWidget(button_loadMap)
     LevelEditor.GUI:addWidget(tf_mapName)
@@ -148,3 +156,22 @@ LevelEditor.activeKeyBinding[" "] = {
 		Tilemap.Camera.target = Tilemap.getActiveScene().characters[1]
 	end
 }
+
+function LevelEditor.renderSelectedTileWidget(self)
+    self:renderBackground()
+    love.graphics.setColor(unpack(self.apparentContainer.fontColor))
+    love.graphics.print(Tilemap.tileNames[LevelEditor.currentTileIndex], self:getLeftAnchor() + 10, self:getTopAnchor()+self:getHeight()/2 - self.apparentContainer.font:getHeight(Tilemap.tileNames[LevelEditor.currentTileIndex])/2)
+
+    -- draw background circle
+    love.graphics.setColor(0,0,0)
+    love.graphics.circle("fill", self:getRightAnchor()-5-Tilemap.Settings.tileSize, self:getTopAnchor() + self:getHeight()/2, Tilemap.Settings.tileSize/2+2)
+
+
+    love.graphics.setStencil(function() love.graphics.circle("fill", self:getRightAnchor()-5-Tilemap.Settings.tileSize, self:getTopAnchor() + self:getHeight()/2, Tilemap.Settings.tileSize/2) end)
+    love.graphics.push()
+    love.graphics.setColor(unpack(Tilemap.tileDict[LevelEditor.currentTileIndex][3]))
+    love.graphics.translate(self:getRightAnchor()-5-Tilemap.Settings.tileSize -Tilemap.Settings.tileSize/2, self:getTopAnchor() + self:getHeight()/2-Tilemap.Settings.tileSize/2)
+    Tilemap.tileDict[LevelEditor.currentTileIndex][1](unpack(Tilemap.tileDict[LevelEditor.currentTileIndex][2]))
+    love.graphics.pop()
+    love.graphics.setStencil()
+end
--- src/Tilemap/Settings.lua
+++ src/Tilemap/Settings.lua
@@ -4,5 +4,5 @@ Tilemap.Settings = {}
 ---
 Tilemap.Settings.levelSize 			= 100
 Tilemap.Settings.tileSize 			= 30
-Tilemap.Settings.playerSize 			= 20
+Tilemap.Settings.playerSize 		= 20
 Tilemap.Settings.playerMoveSpeed 	= 100
